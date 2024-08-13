02461b59221c83b523b8ff2b0ee71d299d0341ad
Purcariu Vlad <purcariuvlad@Purcarius-Mac-mini.local>
2024-08-13 22:33:07 +0200

    created repo

--- modules/repository/locals.tf
+++ modules/repository/locals.tf
@@ -39,6 +39,45 @@ locals {
       uses_template            = false
       template_owner           = ""
       template_repository_name = ""
+    },
+    {
+      name        = "UnifyFootball"
+      description = "A repository made for our beloved Discord BOT written in .NET"
+      topics      = ["discord", "bot", "cs", "gcp", "net"]
+
+      visibility = "public"
+
+      gitignore_template = "" # name as found on https://github.com/github/gitignore
+
+      delete_branch_on_merge = true
+
+      is_template = false
+
+      enable_vulnerability_alerts = true
+      enable_dependabot_updates   = true
+
+      has_issues      = true
+      has_discussions = false
+      has_projects    = true
+      has_wiki        = false
+
+      allows_force_pushes = false
+      force_push_bypassers = [
+        "/RazvanBerbece",
+        "/fhatti"
+      ]
+
+      collaborators = [
+        {
+          username   = "ant-devbot"
+          permission = "admin"
+        }
+      ]
+
+      # If repository should be based off a template, fill these in
+      uses_template            = true
+      template_owner           = "Unify-Solutions"
+      template_repository_name = "BaseTemplate"
     }
   ]
 }
