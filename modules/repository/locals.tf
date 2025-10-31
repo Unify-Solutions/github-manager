1b0eb5fb76a3d218f47110c98ec7fadec014ec20
Antonio Berbece <razvan.berbece@hotmail.com>
2025-10-31 22:37:54 +0000

    repo: `UnifyTickerTool` (#8)

--- modules/repository/locals.tf
+++ modules/repository/locals.tf
@@ -161,6 +161,46 @@ locals {
       template_owner           = "Unify-Solutions"
       template_repository_name = "BaseTemplate"
     },
+    {
+      name        = "UnifyTickerTool"
+      description = "Tool which provides insights into financial markets with a hint of hype."
+      topics      = ["cs", "server", "clients", "finance", "terraform"]
+
+      visibility = "private"
+
+      gitignore_template = "" # name as found on https://github.com/github/gitignore
+
+      delete_branch_on_merge = true
+
+      is_template = false
+
+      enable_vulnerability_alerts = true
+      enable_dependabot_updates   = true
+      enable_secret_scanning      = "disabled"
+
+      has_issues      = true
+      has_discussions = false
+      has_projects    = true
+      has_wiki        = false
+
+      allows_force_pushes = false
+      force_push_bypassers = [
+        "/RazvanBerbece",
+        "/fhatti"
+      ]
+
+      collaborators = [
+        {
+          username   = "ant-devbot"
+          permission = "admin"
+        }
+      ]
+
+      # If repository should be based off a template, fill these in
+      uses_template            = true
+      template_owner           = "Unify-Solutions"
+      template_repository_name = "BaseTemplate"
+    },
   ]
 }
 
